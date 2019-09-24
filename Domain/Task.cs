8ae9d2e2edf16ed62ad9ff553e0a68656d4ae606
Ikhlaaq <ikhlaak@hotmail.com>
2019-09-24 13:00:23 +0200

    Add ability to list tasks

---
+++ Domain/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TODO.Domain
+{
+    class Task
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+
+    
+
+        public Task(string title, DateTime dueDate)
+        {
+            Title = title;
+            DueDate = dueDate;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using TODO.Domain;
 using static System.Console;
+
 
 namespace TODO
 {
     class Program
     {
+
+        static Task[] taskList = new Task[100];
         static void Main(string[] args)
         {
             bool shouldRun = true;
+
+          
 
             while (shouldRun)
             {
@@ -20,9 +26,22 @@ namespace TODO
 
                 ConsoleKeyInfo keyPressed = ReadKey(true);
 
+                Clear();
+
                 switch (keyPressed.Key)
                 {
                     case ConsoleKey.D1:
+
+                        Write("Title:");
+
+                        string title = ReadLine();
+
+                        Write("Due date (yyyy-mm-dd hh:mm): ");
+
+                        DateTime dueDate = DateTime.Parse(ReadLine());
+
+                        taskList[GetIndexPosition()] = new Task(title, dueDate);
+
 
                         break;
 
@@ -33,6 +52,30 @@ namespace TODO
                         break;
                 }
             }
+        }
+
+        static int GetIndexPosition()
+                {
+                    int result = -1;
+                    for (int i = 0; i < taskList.Length; i++)
+                    {
+                        if (taskList[i] != null)
+                        {
+                            continue;
+                        }
+                        if (taskList[i] == null)
+                        {
+                            result = i;
+                            break;
+                        }
+                        if (result == -1)
+                        {
+                            throw new Exception("No avalible position");
+                        }
+                    }
+                    return result;
+
+                }
         }
     }
 }
